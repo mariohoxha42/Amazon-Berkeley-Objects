82ba938d3ab6242cbf4d968902e08cf045ee2e6f
mariohoxha42 <35882837+mariohoxha42@users.noreply.github.com>
2023-12-15 12:11:18 -0600

    test on Git

--- src/main/java/org/mario/process/transform/Output.java
+++ src/main/java/org/mario/process/transform/Output.java
@@ -2,26 +2,27 @@ package org.mario.process.transform;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
 import org.mario.model.raw.AboCoreMetadata;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Output {
 	public void splitOutputs(List<List> masterList, String savePath) {
-		SparkSession spark = SparkSession.builder().master("local")
-				.appName("App")
-//				.config("spark.some.config.option", "some-value")
-				.getOrCreate();
+//		SparkSession sparkSession = SparkSession.builder()
+//				.master("local")
+//				.appName("Spark Session Example")
+//				.getOrCreate();
+//		SQLContext sqlContext = sparkSession.sqlContext();
+		List<Dataset<Row>> finalList = new ArrayList<>();
 		for (List list : masterList) {
 			String processedPath = savePath.replace("generated", "processed");
 			Class<? extends AboCoreMetadata> listClass = (Class<? extends AboCoreMetadata>) list.get(0).getClass();
-			Dataset<Row> listDf = spark.createDataFrame(list, listClass);
+//			Dataset<Row> listDf = sqlContext.createDataFrame(list, listClass);
 			String className = listClass.getName().replace(listClass.getPackageName() + ".", "") + "Df";
 			String fullSavePath = processedPath + "/" + className + ".txt";
-			listDf.show();
-			System.out.println(fullSavePath);
-//			listDf.toJavaRDD().map(Row::toString).saveAsTextFile(fullSavePath);
+//			finalList.add(listDf);
+//			System.out.println(listDf);
 		}
 	}
 }
--- src/test/java/org/mario/tempTransform.java
+++ src/test/java/org/mario/tempTransform.java
@@ -1,6 +1,5 @@
 package org.mario;
 
-import org.apache.spark.sql.SparkSession;
 import org.mario.process.transform.Input;
 import org.mario.process.transform.Output;
 import org.testng.annotations.Test;
@@ -9,6 +8,14 @@ import java.io.IOException;
 import java.util.List;
 
 public class tempTransform {
+//	@Test
+//	public void testSpark() {
+//		SparkSession sparkSession = SparkSession.builder()
+//				.master("local")
+//				.appName("Spark Session Example")
+//				.getOrCreate();
+//	}
+//
 	@Test
 	public void testInput() throws IOException {
 		Input input = new Input();
@@ -16,13 +23,5 @@ public class tempTransform {
 		String inputFolder = "src/test/resources/generated-test-files";
 		List<List> returnList =  input.readFiles(inputFolder);
 		output.splitOutputs(returnList,inputFolder);
-	}
-	
-//	@Test
-	public void testSpark() {
-		SparkSession spark = SparkSession.builder().master("local")
-				.appName("Word Count")
-//				.config("spark.some.config.option", "some-value")
-				.getOrCreate();
 	}
 }
